b0319999bde7eb7586275328eb4567929c0ffe2e
ze561384 <68448899+ze561384@users.noreply.github.com>
2021-03-15 17:47:12 -0500

    **

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { Sequelize } = require('sequelize')
 const app = express()
 
 const corsOptions = {
-  origin: 'http://localhost:3000'
+  origin: '*'
 }
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
